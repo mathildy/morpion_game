66753ebcef3d31c126b00492821750c932c2ce69
TimotheRuffino <62902294+TimotheRuffino@users.noreply.github.com>
2020-04-23 21:26:43 +0200

    Update player.rb

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,9 @@
 class Player 
   attr_reader :name, :value
 
-  def initialize (name, value)
+  def initialize (name, value) #initialiser les noms des joueurs et les valeurs qu'ils peuvent rentrer (X ou O)
     @name = name
-    @value = value
+    @value = value #Value = X ou O
   end 
 
 end
