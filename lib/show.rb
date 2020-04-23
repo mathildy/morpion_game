6487a706717d51bf33dc01d7fe2d223ba5a4266f
TimotheRuffino <62902294+TimotheRuffino@users.noreply.github.com>
2020-04-23 22:37:17 +0200

    Update show.rb

--- lib/show.rb
+++ lib/show.rb
@@ -1,12 +1,1 @@
-require "pry"
-class Show 
-  def start #squelette du tabelau / A1.content etc... sont les positions où seront affichés les X et les O
-    puts "   1   2  3"
-    puts " a #{@A1.content} | #{@A2.content} | #{@A3.content}"
-    puts "   ---------"
-    puts " b #{@B1.content} | #{@B2.content} | #{@B3.content}"
-    puts "   ---------"
-    puts " c #{@C1.content} | #{@C2.content} | #{@C3.content}"
-  end
 
-end
