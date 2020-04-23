3b9047b53125e603b1fa84c211a1a21fc9aee3ec
TimotheRuffino <62902294+TimotheRuffino@users.noreply.github.com>
2020-04-23 21:27:39 +0200

    Update game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -11,15 +11,6 @@ class Game #initiliasier les joueurs et afficher le tableau
     @board = Board.new
   end
 
-  def start #squelette du tabelau / A1.content etc... sont les positions où seront affichés les X et les O
-    puts "   1   2  3"
-    puts " a #{@A1.content} | #{@A2.content} | #{@A3.content}"
-    puts "   ---------"
-    puts " b #{@B1.content} | #{@B2.content} | #{@B3.content}"
-    puts "   ---------"
-    puts " c #{@C1.content} | #{@C2.content} | #{@C3.content}"
-  end
-
 end
 
   
