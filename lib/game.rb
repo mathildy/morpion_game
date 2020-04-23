7d20284b70210c9a968cf70072cbca5c8f52810b
TimotheRuffino <62902294+TimotheRuffino@users.noreply.github.com>
2020-04-23 22:34:30 +0200

    Update game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -1,18 +1,21 @@
-require 'pry'
+class Game
+  def initialize     #TO DO : créé 2 joueurs, créé un board
+    puts"On va jouer au morpion!"
+    puts "le premier qui a un alignement gagne"
+    puts " "
+    puts "Quel le nom du 1er joueur?"
+    player1_name = gets.chomp
+    puts" "
 
-class Game #initiliasier les joueurs et afficher le tableau
-    attr_accessor :players, :board
+    puts "Quel le nom du 2ème joueur?"
+    player2_name = gets.chomp
+    puts" "
 
-  def initialize(player_1, player_2) 
-
-    @players = [Player.new(player_1, "X"), Player.new(player_2, "O")] #initialisation des joueurs
-   
+    @player1 = Player.new(player1_name, "X", "tu n'as pas encore gagné")
+    @player2 = Player.new(player2_name, "O", "tu n'as pas encore gagné")
 
     @board = Board.new
   end
-
-end
-
   
   
 
